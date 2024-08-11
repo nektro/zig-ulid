183f350064ed15148757d512d4641f4337358efa
Meghan Denny <hello@nektro.net>
2024-08-11 00:03:09 -0700

    remove the run step

--- build.zig
+++ build.zig
@@ -5,24 +5,6 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
 
-    const exe = b.addExecutable(.{
-        .name = "zig-ulid",
-        .root_source_file = .{ .path = "main.zig" },
-        .target = target,
-        .optimize = mode,
-    });
-    deps.addAllTo(exe);
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
     const tests = b.addTest(.{
         .root_source_file = b.path("./test.zig"),
         .target = target,
