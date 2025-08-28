9b047a3e3df0e86d47c088d376c7b34ef4801a84
Meghan Denny <hello@nektro.net>
2025-08-28 12:13:41 -0700

    build.zig: ensure tests are run with project root as cwd

--- build.zig
+++ build.zig
@@ -16,6 +16,7 @@ pub fn build(b: *std.Build) void {
     tests.use_lld = !disable_llvm;
 
     const tests_run = b.addRunArtifact(tests);
+    tests_run.setCwd(b.path("."));
     tests_run.has_side_effects = true;
 
     const test_step = b.step("test", "dummy test step to pass CI checks");
